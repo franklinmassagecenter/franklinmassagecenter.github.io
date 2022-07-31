23570e83eb082b10db3d1b02d5d076442abc2fa2
Tommy Frazier <tommy@toefraz.com>
2022-07-31 16:13:04 -0400

    Updating minification

--- prepublish.ps1
+++ prepublish.ps1
@@ -2,6 +2,15 @@ $ErrorActionPreference = "Stop"
 
 Set-Location $PSScriptRoot
 
+(Get-ChildItem -Filter "therapists.min*").FullName -match "therapists.min.(?<Version>\d+).js"
+$Version = [int]$Matches.Version
+$File = "therapists.min.$($Version).js"
+$NewFile = "therapists.min.$($Version + 1).js"
+
+Get-ChildItem -Filter "*.html" -Recurse | ForEach-Object {
+    (Get-Content $_ -Raw).Replace($File, $NewFile) | Set-Content -NoNewLine $_ 
+}
+
 Get-ChildItem -Filter "*.html" -File -Path $PSScriptRoot | ForEach-Object {
     $base = $_.BaseName
 
@@ -13,4 +22,6 @@ Get-ChildItem -Filter "*.html" -File -Path $PSScriptRoot | ForEach-Object {
     Copy-Item $_.FullName -Destination (Join-Path $PSScriptRoot -ChildPath "$base\index.html") -Force
 }
 
-npx uglify-js therapists.js -o therapists.min.js
+npx uglify-js therapists.js -o $NewFile
+
+Remove-Item $File
