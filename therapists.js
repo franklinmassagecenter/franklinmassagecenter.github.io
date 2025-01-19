560ca3c790b893b4b7af304c3b3dc94092f1fb59
Tommy Frazier <tommy@toefraz.com>
2025-01-19 17:27:14 -0500

    Website updates

--- therapists.js
+++ therapists.js
@@ -7,12 +7,14 @@ window.THERAPIES = {
   Deep: "Deep Tissue",
   Energetic: "Energetic Healing",
   Graston: "Graston Technique",
+  KT: "KT Tape",
   Lymphatic: "Lymphatic Drainage",
   Myofascial: "Myofascial Release",
   Neuromuscular: "Neuromuscular",
   Prenatal: "Prenatal Massage",
   Reiki: "Reiki",
   Scar: "Scar Recovery",
+  Scrape: "Scrape & Tape",
   Sports: "Sports",
   Stretching: "Stretching",
   Swedish: "Swedish",
@@ -33,7 +35,7 @@ window.therapists = [
       THERAPIES.Graston,
       THERAPIES.Sports,
       THERAPIES.Swedish,
-      THERAPIES.TMJ
+      THERAPIES.TMJ,
     ],
   },
   {
@@ -68,7 +70,7 @@ window.therapists = [
       THERAPIES.Graston,
       THERAPIES.Prenatal,
       THERAPIES.Energetic,
-      THERAPIES.Reiki
+      THERAPIES.Reiki,
     ],
   },
   {
@@ -77,21 +79,44 @@ window.therapists = [
       "Carrie graduated from Birmingham School of Massage (Birmingham, AL) in February of 2014. She promotes wellness through personalized pain management and stress reduction therapy, and tailors each session to the client's specific needs/goals. In most therapy sessions, Carrie utilizes Swedish, Deep Tissue, & Neuromuscular techniques for a full body massage, to produce an \"all-encompassing\" therapeutic outcome.",
       "Carrie is passionate about combining her skills to produce both physical and psychological benefits for her clients, and believes in utilizing massage therapy for treatment of anxiety and depression as well. She prides herself in assisting not only physical healing, but also mental and emotional healing by helping the brain produce those \"happy chemicals\" via massage therapy."
     ],
-    therapies: [THERAPIES.Deep, THERAPIES.Neuromuscular, THERAPIES.Swedish, THERAPIES.Prenatal, THERAPIES.Sports],
+    therapies: [
+      THERAPIES.Deep, 
+      THERAPIES.Neuromuscular, 
+      THERAPIES.Swedish, 
+      THERAPIES.Prenatal, 
+      THERAPIES.Sports,
+    ],
   },
   {
     name: "Zoë Greene",
     bio: [
       "Hi, my name is Zoë! My goal is to provide a supportive space for my clients to find their inner relaxation so they can live more fully and connected to their body. I love helping people create balance within their bodies through the power of massage therapy. I graduated from the Nashville School of Massage Therapy where I received an academic award of excellence for my work. I have also trained in neuromuscular massage therapy at the Cumberland Institute of Holistic Therapies in order to help clients with muscle pain management. I believe through calming touch, I can offer a sense of peace and well-being for all my clients.",
     ],
-    therapies: [THERAPIES.Deep, THERAPIES.Myofascial, THERAPIES.Neuromuscular, THERAPIES.Stretching, THERAPIES.Swedish],
+    therapies: [
+      THERAPIES.Deep, 
+      THERAPIES.Myofascial, 
+      THERAPIES.Neuromuscular, 
+      THERAPIES.Stretching, 
+      THERAPIES.Swedish, 
+      THERAPIES.Ashiatsu,
+    ],
   },
   {
     name: "Laura Stewart",
     bio: [
       "Laura creates a safe and nurturing environment for clients to experience profound relaxation and therapeutic restoration. Together she will work with you on a customized plan that is uniquely suited to your needs. Her approach to treatment includes craniosacral therapy, myofascial release and Reiki.  Treatments center on gently releasing restrictions from the body to help restore balance and function. She loves helping people who live with chronic pain are experiencing immune or nervous system disorders, migraine, neck pain, tmj disorder, injury, trauma, tension, anxiety, depression, and other disorders.",
     ],
-    therapies: [THERAPIES.Craniosacral, THERAPIES.Myofascial, THERAPIES.Reiki, THERAPIES.Deep, THERAPIES.Sports, THERAPIES.Swedish, THERAPIES.TMJ, THERAPIES.Prenatal],
+    therapies: [
+      THERAPIES.Craniosacral, 
+      THERAPIES.Myofascial, 
+      THERAPIES.Reiki, 
+      THERAPIES.Deep, 
+      THERAPIES.Sports, 
+      THERAPIES.Swedish, 
+      THERAPIES.TMJ, 
+      THERAPIES.Prenatal, 
+      THERAPIES.Graston,
+    ],
   },
   {
     name: "Tia Day",
@@ -99,13 +124,76 @@ window.therapists = [
       "Tia graduated from Cumberland Institute of Holistic Therapies in Brentwood, TN. She started out as a strength and conditioning coach in Colorado working with prospective D1 athletes and tactical personnel. She gradually moved into therapeutic bodywork as a flexibility coach and Muscle Activation Techniques (MAT®) specialist.",
       "She enjoys working with a diverse clientele in an integrative way to bring relief and connection back into their body. She has experience in Swedish, Deep Tissue, Acupressure, Manual Lymphatic Drainage, and Sports massage.",
     ],
-    therapies: [THERAPIES.Deep, THERAPIES.Swedish, THERAPIES.Acupressure, THERAPIES.Lymphatic, THERAPIES.Sports, THERAPIES.Cupping, THERAPIES.Myofascial],
+    therapies: [
+      THERAPIES.Deep, 
+      THERAPIES.Swedish, 
+      THERAPIES.Acupressure, 
+      THERAPIES.Lymphatic, 
+      THERAPIES.Sports, 
+      THERAPIES.Cupping, 
+      THERAPIES.Myofascial, 
+      THERAPIES.Graston, 
+      THERAPIES.Acupressure, 
+      THERAPIES.KT, 
+      THERAPIES.Scrape,
+    ],
   },
   {
     name: "Luna Roberts",
     bio: [
       "Hey there, I'm Luna! I am a graduate of Cumberland Institute of Holistic Therapies in Brentwood, Tennessee. I got into the massage therapy field as a way to heal my own body, and I am very passionate about being able to use my skills to promote recovery to others. I am eager to keep growing my techniques to improve the health and injury of people. I enjoy bringing a nurturing and relaxing state of mind to be able to connect the body to healing in a holistic way.",
     ],
-    therapies: [THERAPIES.Deep, THERAPIES.Swedish, THERAPIES.Acupressure, THERAPIES.Craniosacral],
+    therapies: [
+      THERAPIES.Deep, 
+      THERAPIES.Swedish, 
+      THERAPIES.Acupressure, 
+      THERAPIES.Craniosacral, 
+      THERAPIES.Sports, 
+      THERAPIES.Cupping,
+    ]
+  },
+  {
+    name: "Mallory Barbee",
+    bio: [
+      "Mallory graduated from the Nashville School of Massage in 2017. As a life-long athlete, she recognized that pursuing a career in massage therapy would allow her to help clients achieve their fitness goals while minimizing the risk of injury. Over the past eight years, Mallory has worked with a diverse clientele, including professional athletes from the NFL, NHL, and MLB, as well as stylists, surgeons, teachers, and individuals seeking relaxation.",
+      "Mallory has developed a deep passion for cupping therapy and its healing benefits on the body. While cupping is one of her preferred modalities, she also specializes in deep tissue, Swedish, sports massage, and relaxation techniques. Each session is tailored to meet the specific needs of her clients, often incorporating a combination of different modalities.",
+      "Known for her attentive nature, Mallory actively listens to her clients and assesses their unique needs. She customizes each session based on their condition that day, focusing on pain management and overall well-being. ",
+      "Beyond her professional pursuits, Mallory enjoys spending quality time with her wife, son, and their three dogs. She is also an avid fan of MMA, and when she is not engaged in woodworking or home improvement projects, she can often be found watching MMA events. Mallory looks forward to meeting new clients and building lasting relationships with them!",
+    ],
+    therapies: [
+      THERAPIES.Deep, 
+      THERAPIES.Swedish, 
+      THERAPIES.Sports, 
+      THERAPIES.Cupping,
+    ],
+  },
+  {
+    name: "Rookh McClellan",
+    bio: [
+      "Hi, I'm Rookh, a certified massage therapist and a proud graduate of the Cumberland Institute of Holistic Therapies (Class of 2019). I'm passionate about providing personalized treatments that promote relaxation, healing, and overall wellness. Whether you're seeking relief from stress, muscle tension, or physical discomfort, I create a calming environment to support your body and mind.,",
+      "Let's work together to help you feel balanced, restored, and renewed."
+    ],
+    therapies: [
+      THERAPIES.Deep,
+      THERAPIES.Swedish,
+      THERAPIES.Sports,
+      THERAPIES.Cupping,
+    ],
+  },
+  {
+    name: "Sharunda Franklin",
+    bio: [
+      "Sharunda's desire to help people heal was sparked when she spent 10 years as a rehabilitation technician working with patients suffering from strokes, brain, and spinal injuries. This led her on a journey to approach healing in a more holistic way: understanding that the mind and body are connected.",
+      "She graduated as valedictorian from the Institute of Health and Tech. in 2018. She also worked as a medical massage therapist where she learned a variety of techniques and stretches to help relieve the body of tension and stress.",
+    ],
+    therapies: [
+      THERAPIES.Abdominal,
+      THERAPIES.Deep,
+      THERAPIES.Ashiatsu,
+      THERAPIES.Cupping,
+      THERAPIES.Swedish,
+      THERAPIES.Sports,
+      THERAPIES.Prenatal,
+    ],
   },
 ];
